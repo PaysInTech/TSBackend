c0d0c60d60d5e318a6355fc78d96e1e2450232fb
Shreyas <shreyaspatilg@gmail.com>
2021-10-09 12:15:29 +0530

    Add exceptions model

--- api/src/main/kotlin/app/techsalaries/exception/ApiException.kt
+++ api/src/main/kotlin/app/techsalaries/exception/ApiException.kt
@@ -1,10 +1,21 @@
 package app.techsalaries.exception
 
-open class BadRequestException(override val message: String) : RuntimeException()
+import app.techsalaries.api.response.Unsuccessful
+import io.ktor.http.HttpStatusCode
 
-open class NotFoundException(override val message: String) : RuntimeException()
+sealed class UnsatisfiedRequestException(
+    override val message: String,
+    val statusCode: HttpStatusCode
+) : RuntimeException() {
+    val response get() = Unsuccessful(this, statusCode)
+}
 
-open class AuthenticationException(override val message: String) : RuntimeException()
-open class AuthorizationException(override val message: String) : RuntimeException()
 
-open class ServerError(override val message: String) : RuntimeException()
+open class BadRequestException(override val message: String) : UnsatisfiedRequestException(message, HttpStatusCode.BadRequest)
+
+open class NotFoundException(override val message: String) : UnsatisfiedRequestException(message, HttpStatusCode.NotFound)
+
+open class AuthenticationException(override val message: String) : UnsatisfiedRequestException(message, HttpStatusCode.Unauthorized)
+open class AuthorizationException(override val message: String) : UnsatisfiedRequestException(message, HttpStatusCode.Forbidden)
+
+open class ServerError(override val message: String) : UnsatisfiedRequestException(message, HttpStatusCode.InternalServerError)
