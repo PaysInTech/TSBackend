47d5995aa1f7ccf70463c06da01156f64233be6c
Shreyas <shreyaspatilg@gmail.com>
2021-10-10 22:12:56 +0530

    Reformat with spotless

--- api/src/main/kotlin/app/techsalaries/api/health/HealthApi.kt
+++ api/src/main/kotlin/app/techsalaries/api/health/HealthApi.kt
@@ -1,13 +1,10 @@
 package app.techsalaries.api.health
 
 import app.techsalaries.api.TechSalariesApiRoute
-import app.techsalaries.api.health.model.HealthResponse
 import app.techsalaries.plugins.controllers
 import app.techsalaries.utils.returnResponse
-import io.ktor.application.call
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.get
-import io.ktor.response.respond
 import io.ktor.routing.Route
 
 @OptIn(KtorExperimentalLocationsAPI::class)
--- api/src/main/kotlin/app/techsalaries/di/component/ControllerComponent.kt
+++ api/src/main/kotlin/app/techsalaries/di/component/ControllerComponent.kt
@@ -2,7 +2,6 @@ package app.techsalaries.di.component
 
 import app.techsalaries.api.health.HealthController
 import app.techsalaries.api.info.InfoController
-import dagger.Lazy
 import dagger.Subcomponent
 import javax.inject.Singleton
 
