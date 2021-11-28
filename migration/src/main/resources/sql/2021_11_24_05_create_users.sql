9827ccbbdf6349310869771a3ff159be5d6fede9
Shreyas <shreyaspatilg@gmail.com>
2021-11-28 16:24:10 +0530

    Add index on username and email

--- migration/src/main/resources/sql/2021_11_24_05_create_users.sql
+++ migration/src/main/resources/sql/2021_11_24_05_create_users.sql
@@ -5,4 +5,10 @@ CREATE TABLE users
     email      VARCHAR     NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+);
+
+CREATE INDEX IF NOT EXISTS users_username_idx
+    ON users (username);
+
+CREATE INDEX IF NOT EXISTS users_email_idx
+    ON users (email);
