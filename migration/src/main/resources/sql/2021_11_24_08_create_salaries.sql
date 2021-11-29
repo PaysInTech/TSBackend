52c5919d03254fb8886524bbc5e06b6709f9d425
Shreyas <shreyaspatilg@gmail.com>
2021-11-29 21:57:56 +0530

    Add currency in salaries schema

--- migration/src/main/resources/sql/2021_11_24_08_create_salaries.sql
+++ migration/src/main/resources/sql/2021_11_24_08_create_salaries.sql
@@ -3,6 +3,7 @@ CREATE TABLE salaries
     id               BIGSERIAL PRIMARY KEY,
     job_detail_id    BIGINT,
     coins            INT         NOT NULL,
+    currency         VARCHAR(5)  NOT NULL,
     user_id          VARCHAR     REFERENCES users (id) ON DELETE SET NULL,
     metadata         JSONB,
     metadata_version INT,
