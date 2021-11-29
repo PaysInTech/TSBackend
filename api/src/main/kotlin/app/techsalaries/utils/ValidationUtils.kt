e425adb3ec43d8860a08040dae547184d17f7c6a
Shreyas <shreyaspatilg@gmail.com>
2021-11-29 21:58:33 +0530

    Add validation utility for validating email

--- api/src/main/kotlin/app/techsalaries/utils/ValidationUtils.kt
+++ api/src/main/kotlin/app/techsalaries/utils/ValidationUtils.kt
@@ -1,6 +1,7 @@
 package app.techsalaries.utils
 
 import app.techsalaries.exception.BadRequestException
+import java.util.regex.Pattern
 
 /**
  * If given [value] is false then throws [BadRequestException] with specified [lazyMessage]
@@ -10,3 +11,8 @@ inline fun validate(value: Boolean, lazyMessage: () -> Any? = { "" }) {
         throw BadRequestException(lazyMessage().toString())
     }
 }
+
+fun isValidEmail(email: String): Boolean = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$"
+    .toPattern(Pattern.CASE_INSENSITIVE)
+    .matcher(email)
+    .matches()
