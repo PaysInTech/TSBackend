700eb216a538d245772b56fd26585a54ef5783b7
Shreyas <shreyaspatilg@gmail.com>
2021-11-29 22:09:02 +0530

    Refactor repositories: use raw queries

--- port/db/src/main/kotlin/app/techsalaries/db/jobInfo/JobInfoRepositoryImpl.kt
+++ port/db/src/main/kotlin/app/techsalaries/db/jobInfo/JobInfoRepositoryImpl.kt
@@ -6,60 +6,35 @@ import app.techsalaries.core.jobInfo.model.ContributionLevel
 import app.techsalaries.core.jobInfo.model.JobProfile
 import app.techsalaries.core.jobInfo.model.ProgrammingLanguage
 import app.techsalaries.core.jobInfo.model.Technology
-import app.techsalaries.db.schema.ContributionLevels
-import app.techsalaries.db.schema.JobProfiles
-import app.techsalaries.db.schema.ProgrammingLanguages
-import app.techsalaries.db.schema.Technologies
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
-import org.ktorm.database.Database
-import org.ktorm.dsl.eq
-import org.ktorm.dsl.from
-import org.ktorm.dsl.map
-import org.ktorm.dsl.select
-import org.ktorm.dsl.where
+import app.techsalaries.db.jobInfo.query.GetAllContributionLevels
+import app.techsalaries.db.jobInfo.query.GetAllJobProfilesQuery
+import app.techsalaries.db.jobInfo.query.GetAllProgrammingLanguages
+import app.techsalaries.db.jobInfo.query.GetAllTechnologies
 import javax.inject.Inject
 import javax.inject.Singleton
+import javax.sql.DataSource
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 
 @Singleton
 class JobInfoRepositoryImpl @Inject constructor(
-    private val database: Database,
+    private val dataSource: DataSource,
     @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : JobInfoRepository {
 
     override suspend fun getAllJobProfiles(): List<JobProfile> = withContext(dispatcher) {
-        database
-            .from(JobProfiles)
-            .select()
-            .where { JobProfiles.enabled eq true }
-            .map { JobProfiles.createEntity(it) }
-            .map { JobProfile(it.id, it.name) }
+        GetAllJobProfilesQuery().executeAsList(dataSource, Unit)
     }
 
     override suspend fun getAllProgrammingLanguages(): List<ProgrammingLanguage> = withContext(dispatcher) {
-        database
-            .from(ProgrammingLanguages)
-            .select()
-            .where { ProgrammingLanguages.enabled eq true }
-            .map { ProgrammingLanguages.createEntity(it) }
-            .map { ProgrammingLanguage(it.id, it.name) }
+        GetAllProgrammingLanguages().executeAsList(dataSource, Unit)
     }
 
     override suspend fun getAllTechnologies(): List<Technology> = withContext(dispatcher) {
-        database
-            .from(Technologies)
-            .select()
-            .where { Technologies.enabled eq true }
-            .map { Technologies.createEntity(it) }
-            .map { Technology(it.id, it.name) }
+        GetAllTechnologies().executeAsList(dataSource, Unit)
     }
 
     override suspend fun getAllContributionLevels(): List<ContributionLevel> = withContext(dispatcher) {
-        database
-            .from(ContributionLevels)
-            .select()
-            .where { ContributionLevels.enabled eq true }
-            .map { ContributionLevels.createEntity(it) }
-            .map { ContributionLevel(it.id, it.name) }
+        GetAllContributionLevels().executeAsList(dataSource, Unit)
     }
 }
--- port/db/src/main/kotlin/app/techsalaries/db/salary/SalaryRepositoryImpl.kt
+++ port/db/src/main/kotlin/app/techsalaries/db/salary/SalaryRepositoryImpl.kt
@@ -5,138 +5,95 @@ import app.techsalaries.core.salary.SalaryRepository
 import app.techsalaries.core.salary.model.AddSalary
 import app.techsalaries.core.salary.model.AddSalaryDetail
 import app.techsalaries.core.salary.model.AdditionalBenefits
-import app.techsalaries.db.Config
-import app.techsalaries.db.schema.JobBenefits
-import app.techsalaries.db.schema.JobDetails
-import app.techsalaries.db.schema.JobProgrammingLanguages
-import app.techsalaries.db.schema.JobTechnologies
-import app.techsalaries.db.schema.ProgrammingLanguages
-import app.techsalaries.db.schema.Salaries
-import app.techsalaries.db.schema.Technologies
+import app.techsalaries.db.jobInfo.query.GetAllContributionLevels
+import app.techsalaries.db.jobInfo.query.GetProgrammingLanguagesByIdsQuery
+import app.techsalaries.db.jobInfo.query.GetTechnologiesByIdsQuery
+import app.techsalaries.db.salary.query.AddJobBenefitCommand
+import app.techsalaries.db.salary.query.AddJobDetailsQuery
+import app.techsalaries.db.salary.query.AddJobProgrammingLanguageCommand
+import app.techsalaries.db.salary.query.AddJobTechnologyCommand
+import app.techsalaries.db.salary.query.AddSalaryQuery
+import app.techsalaries.db.sql.executeTransaction
+import java.sql.Connection
 import javax.inject.Inject
 import javax.inject.Singleton
+import javax.sql.DataSource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
-import org.ktorm.database.Database
-import org.ktorm.dsl.from
-import org.ktorm.dsl.inList
-import org.ktorm.dsl.map
-import org.ktorm.dsl.select
-import org.ktorm.dsl.where
-import org.ktorm.support.postgresql.bulkInsert
-import org.ktorm.support.postgresql.insertReturning
 
 @Singleton
 class SalaryRepositoryImpl @Inject constructor(
-    private val database: Database,
+    private val dataSource: DataSource,
     @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : SalaryRepository {
     override suspend fun addSalary(salary: AddSalary): Long = withContext(dispatcher) {
         val salaryDetail = salary.detail
-        database.useTransaction {
-            val jobDetailId = addJobDetails(salaryDetail) ?: error("Unable to add job details")
+        dataSource.executeTransaction { connection ->
+            salaryDetail.contributionLevel?.let { checkExistContributionLevelOrThrow(connection, it) }
+
+            val jobDetailId = addJobDetails(connection, salaryDetail) ?: error("Unable to add job details")
 
             salaryDetail.technologies?.let { technologies ->
-                checkExistTechnologiesOrThrow(technologies)
-                addJobTechnologies(jobDetailId, technologies)
+                checkExistTechnologiesOrThrow(connection, technologies)
+                addJobTechnologies(connection, jobDetailId, technologies)
             }
             salaryDetail.programmingLanguagesUsed?.let { programmingLanguages ->
-                checkExistProgrammingLanguagesOrThrow(programmingLanguages)
-                addJobProgrammingLanguages(jobDetailId, programmingLanguages)
+                checkExistProgrammingLanguagesOrThrow(connection, programmingLanguages)
+                addJobProgrammingLanguages(connection, jobDetailId, programmingLanguages)
             }
-            salaryDetail.additionalBenefits?.benefits?.let { addJobBenefits(jobDetailId, it) }
-            addSalary(jobDetailId, salary) ?: error("Unable to add salary details")
-        }
+            salaryDetail.additionalBenefits?.benefits?.let { addJobBenefits(connection, jobDetailId, it) }
+            addSalary(connection, jobDetailId, salary) ?: error("Unable to add salary details")
+        }.getOrNull() ?: error("Failed to add salary details")
     }
 
-    private fun addSalary(jobDetailId: Long, salary: AddSalary): Long? =
-        database.insertReturning(Salaries, Salaries.id) {
-            set(it.jobDetailId, jobDetailId)
-            set(it.coins, salary.coins)
-            set(it.metadata, salary.metadata)
-            set(it.metadataVersion, Config.SALARY_METADATA_VERSION)
-        }
+    private fun addSalary(connection: Connection, jobDetailId: Long, salary: AddSalary): Long? =
+        AddSalaryQuery().executeAsOneOrNull(connection, jobDetailId to salary)
 
-    private fun addJobDetails(salaryDetail: AddSalaryDetail): Long? =
-        database.insertReturning(JobDetails, JobDetails.id) {
-            set(it.companyName, salaryDetail.company)
-            set(it.designation, salaryDetail.designation)
-            set(it.profileId, salaryDetail.profileId)
-            set(it.contributionLevelId, salaryDetail.contributionLevel)
-            set(it.level, salaryDetail.level?.name)
-            set(it.team, salaryDetail.team)
-            set(it.annualCtc, salaryDetail.salary.annualCTC)
-            set(it.inhandPerMonth, salaryDetail.salary.inhandPerMonth)
-            set(it.bonus, salaryDetail.salary.bonus)
-            set(it.joiningDate, salaryDetail.joiningDate)
-            set(it.lastWorkingDate, salaryDetail.lastWorkingDate)
-            set(it.isPromoted, salaryDetail.isPromoted)
-            set(it.workType, salaryDetail.workType.value)
-            set(it.locationState, salaryDetail.baseLocation?.state)
-            set(it.locationCity, salaryDetail.baseLocation?.city)
-        }
+    private fun addJobDetails(connection: Connection, salaryDetail: AddSalaryDetail): Long? =
+        AddJobDetailsQuery().executeAsOneOrNull(connection, salaryDetail)
 
-    private fun addJobTechnologies(jobDetailId: Long, technologies: Set<Long>) = database.bulkInsert(JobTechnologies) {
-        technologies.forEach { technologyId ->
-            item {
-                set(it.jobDetailId, jobDetailId)
-                set(it.technologyId, technologyId)
-            }
-        }
-    }
+    private fun addJobTechnologies(connection: Connection, jobDetailId: Long, technologies: Set<Long>) =
+        AddJobTechnologyCommand().executeBatch(connection, technologies.map { jobDetailId to it })
 
     private fun addJobProgrammingLanguages(
+        connection: Connection,
         jobDetailId: Long,
         programmingLanguages: Set<Long>
-    ) = database.bulkInsert(JobProgrammingLanguages) {
-        programmingLanguages.forEach { languageId ->
-            item {
-                set(it.jobDetailId, jobDetailId)
-                set(it.programmingLanguageId, languageId)
-            }
-        }
-    }
+    ) = AddJobProgrammingLanguageCommand().executeBatch(connection, programmingLanguages.map { jobDetailId to it })
 
     private fun addJobBenefits(
+        connection: Connection,
         jobDetailId: Long,
         benefits: Set<AdditionalBenefits.Benefit>
-    ) = database.bulkInsert(JobBenefits) {
-        benefits.forEach { benefit ->
-            item {
-                set(it.jobDetailId, jobDetailId)
-                set(it.type, benefit.type)
-                set(it.amount, benefit.amount)
-            }
-        }
-    }
+    ) = AddJobBenefitCommand().executeBatch(connection, benefits.map { jobDetailId to it })
 
-    private fun checkExistTechnologiesOrThrow(technologyIds: Set<Long>) {
-        val existedTechnologies = database
-            .from(Technologies)
-            .select(Technologies.id)
-            .where { Technologies.id inList technologyIds }
-            .map { it[Technologies.id] }
-            .toSet()
+    private fun checkExistTechnologiesOrThrow(connection: Connection, technologyIds: Set<Long>) {
+        val existedTechnologies = GetTechnologiesByIdsQuery(technologyIds)
+            .executeAsList(connection, Unit).map { it.id }.toSet()
 
         val nonExistingTechnologyIds = technologyIds - existedTechnologies
-
+        println("ETS: $existedTechnologies")
         if (nonExistingTechnologyIds.isNotEmpty()) {
             error("Technology IDs $nonExistingTechnologyIds not exists in the system")
         }
     }
 
-    private fun checkExistProgrammingLanguagesOrThrow(programmingLanguageIds: Set<Long>) {
-        val existedProgrammingLanguages = database
-            .from(ProgrammingLanguages)
-            .select(ProgrammingLanguages.id)
-            .where { ProgrammingLanguages.id inList programmingLanguageIds }
-            .map { it[ProgrammingLanguages.id] }
-            .toSet()
+    private fun checkExistProgrammingLanguagesOrThrow(connection: Connection, programmingLanguageIds: Set<Long>) {
+        val existedProgrammingLanguages = GetProgrammingLanguagesByIdsQuery(programmingLanguageIds)
+            .executeAsList(connection, Unit).map { it.id }.toSet()
 
         val nonExistingLanguageIds = programmingLanguageIds - existedProgrammingLanguages
 
         if (nonExistingLanguageIds.isNotEmpty()) {
             error("Programming language IDs $nonExistingLanguageIds not exists in the system")
+        }
+    }
+
+    private fun checkExistContributionLevelOrThrow(connection: Connection, levelId: Long) {
+        val existedContributionLevels = GetAllContributionLevels().executeAsList(connection, Unit).map { it.id }
+
+        if (levelId !in existedContributionLevels) {
+            error("Contribution Level ID '$levelId' not exists in the system")
         }
     }
 }
