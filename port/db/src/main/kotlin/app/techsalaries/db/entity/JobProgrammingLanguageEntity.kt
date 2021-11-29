dcfd12df4bf4446ee4e4316a777fd9bdbdfe74f1
Shreyas <shreyaspatilg@gmail.com>
2021-11-29 22:02:57 +0530

    Remove ktorm schema and entities

--- port/db/src/main/kotlin/app/techsalaries/db/entity/JobProgrammingLanguageEntity.kt
+++
@@ -1,9 +0,0 @@
-package app.techsalaries.db.entity
-
-import org.ktorm.entity.Entity
-
-interface JobProgrammingLanguageEntity : Entity<JobProgrammingLanguageEntity> {
-    val id: Long
-    val jobDetail: JobDetailEntity
-    val programmingLanguage: ProgrammingLanguageEntity
-}
--- port/db/src/main/kotlin/app/techsalaries/db/entity/JobTechnologyEntity.kt
+++
@@ -1,9 +0,0 @@
-package app.techsalaries.db.entity
-
-import org.ktorm.entity.Entity
-
-interface JobTechnologyEntity : Entity<JobTechnologyEntity> {
-    val id: Long
-    val jobDetail: JobDetailEntity
-    val technology: TechnologyEntity
-}
