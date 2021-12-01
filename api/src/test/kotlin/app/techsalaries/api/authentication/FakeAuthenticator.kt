c0b6bf46711a3db44000f262b712bb39a4d30b90
Shreyas <shreyaspatilg@gmail.com>
2021-12-01 23:23:02 +0530

    Fix test authenticator

--- api/src/test/kotlin/app/techsalaries/api/authentication/FakeAuthenticator.kt
+++ api/src/test/kotlin/app/techsalaries/api/authentication/FakeAuthenticator.kt
@@ -1,24 +1,27 @@
 package app.techsalaries.api.authentication
 
 import app.techsalaries.core.user.model.User
+import app.techsalaries.core.user.model.UserRole
 import app.techsalaries.exception.CommonErrors
 import app.techsalaries.exception.authenticationError
 import javax.inject.Inject
 
 class FakeAuthenticator @Inject constructor() : Authenticator {
-    private var user: User? = null
-
     override suspend fun authenticate(token: String): User =
-        if (token == VALID_TOKEN && user != null) {
-            user!!
+        if (token == "valid-token") {
+            AuthenticatedUser
         } else {
             authenticationError(CommonErrors.INVALID_AUTHENTICATION_TOKEN)
         }
-
-    fun provideFakeUser(user: User?) {
-        this.user = user
-    }
 }
+
+val AuthenticatedUser = User(
+    id = "user1234",
+    username = "user1234",
+    email = "example@email.com",
+    totalCoins = 1000,
+    roles = listOf(UserRole.MEMBER)
+)
 
 val VALID_TOKEN = "Bearer valid-token"
 val INVALID_TOKEN = "Bearer invalid-token"
